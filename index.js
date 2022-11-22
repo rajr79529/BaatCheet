96864aa14d31d10f1753234146a4e290f6ca0146
Rahul Kumar <rajr79529@gmail.com>
2022-11-22 22:31:55 +0530

    setup ejs and create views

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require("express");
-const app = express();
+const path = require("path");
 const port = 8000;
 
+const app = express();
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname + "/views"));
 app.use("/", require("./routes"));
 
 app.listen(port, function (err) {
