01d87b9995affb4e0b53aa778630d77789971891
Rahul Kumar <rajr79529@gmail.com>
2022-11-22 12:12:27 +0530

    add controller for users profile and posts

---
+++ controllers/users_controller.js
@@ -0,0 +1,7 @@
+module.exports.profile = function (req, res) {
+   res.send("<h1>Users Profile</h1>");
+};
+
+module.exports.posts = function (req, res) {
+   res.send("<h1>THis is Posts section</h1>");
+};
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const homeController = require("../controllers/homeController");
+const homeController = require("../controllers/home_controller");
 
 console.log("Index from Routes");
 router.get("/", homeController.home);
+
+//for any further routes use below syntax
+router.use("/users", require("./users"));
 
 module.exports = router;
