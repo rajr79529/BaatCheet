93c4e198220b27f918b280ffa75bc812703aaa56
Rahul Kumar <rajr79529@gmail.com>
2022-11-23 22:07:56 +0530

    create partials and layout

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const path = require("path");
+const ejsLayout = require("express-ejs-layouts");
 const port = 8000;
 
 const app = express();
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname + "/views"));
+
+app.use(ejsLayout);
 app.use("/", require("./routes"));
 
 app.listen(port, function (err) {
