12ab833bd0f3ce426a862aeb09f5f09377018109
Rahul Kumar <rajr79529@gmail.com>
2022-11-23 22:38:00 +0530

    add extractStyle and extractScript for individual components

--- index.js
+++ index.js
@@ -8,7 +8,12 @@ const app = express();
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname + "/views"));
 
+app.use(express.static("./assets"));
 app.use(ejsLayout);
+//to extract styles and scripts from files
+app.set("layout extractScripts", true);
+app.set("layout extractStyles", true);
+
 app.use("/", require("./routes"));
 
 app.listen(port, function (err) {
