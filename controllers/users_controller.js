ae86ef1b104f77931e6ecf2736b0829ffca31907
Rahul Kumar <rajr79529@gmail.com>
2022-11-25 22:09:39 +0530

    manual authentication,render user detail and logout functionality

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -2,7 +2,24 @@ const UserDb = require("../models/user");
 
 // render profile page
 module.exports.profile = function (req, res) {
-   return res.render("user_profile", { title: "profile" });
+   if (req.cookies.user_id) {
+      UserDb.findById(req.cookies.user_id, (err, user) => {
+         if (err) {
+            console.log("Error in finding if user exist or not");
+         }
+         if (user) {
+            return res.render("user_profile", {
+               title: "profile",
+               user,
+            });
+         } else {
+            return res.redirect("/users/sign-in");
+         }
+      });
+   } else {
+      return res.redirect("/users/sign-in");
+   }
+
    //    res.send("<h1>Users Profile</h1>");
 };
 
@@ -46,5 +63,32 @@ module.exports.create = function (req, res) {
 
 // create session for user
 module.exports.createSession = function (req, res) {
-   //TODO
+   //find User
+   UserDb.findOne({ email: req.body.email }, (error, user) => {
+      if (error) {
+         console.log("Error in finding user in sign-in");
+      }
+      //user found
+      if (user) {
+         //password matched
+         if (user.password === req.body.password) {
+            //save cookie
+            res.cookie("user_id", user.id);
+            //redirect to profile page
+            res.redirect("/users/profile");
+         } else {
+            //password not matched
+            res.redirect("back");
+         }
+      } else {
+         //user not found
+         res.redirect("back");
+      }
+   });
+};
+
+//sign-out route
+module.exports.signOut = function (req, res) {
+   res.clearCookie("user_id");
+   res.redirect("/users/sign-in");
 };
