7aa8b331cabc635d2ad09ef73a93b474c171a94a
Rahul Kumar <rajr79529@gmail.com>
2022-12-06 10:42:03 +0530

    add comments and username for each post, style to make footer at the bottom.

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -12,6 +12,12 @@ module.exports.home = function (req, res) {
    //populate will replace the objectId with the whole user object.
    Post.find()
       .populate("user")
+      .populate({
+         path: "comments",
+         populate: {
+            path: "user",
+         },
+      })
       .exec((err, posts) => {
          if (err) {
             console.log("Error in finding posts", err);
